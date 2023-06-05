0c6f7857375b4550fd2ac67685e65a94130f7ca7
cliff <clifflee1022@gmail.com>
2023-06-05 17:39:33 +0800

    feat: sidebar ok

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter as Router } from "react-router-dom"
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react"
+
 import App from "./App"
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
